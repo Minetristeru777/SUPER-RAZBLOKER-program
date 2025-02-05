2a69eb5338a448a27633df366db47cf4d7e0bbfa
Minetristeru777 <dorda.ivan14@gmail.com>
2025-02-05 15:40:16 +0200

    Add files via upload

---
+++ src/app/forms/MainForm.php
@@ -0,0 +1,46 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class MainForm extends AbstractForm
+{
+
+    /**
+     * @event button6.click-Left 
+     */
+    function doButton6ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button7.click-Left 
+     */
+    function doButton7ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button.click-Left 
+     */
+    function doButtonClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/app/forms/ProgramsForm.php
@@ -0,0 +1,74 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class ProgramsForm extends AbstractForm
+{
+
+    /**
+     * @event button.click-Left 
+     */
+    function doButtonClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event buttonAlt.click-Left 
+     */
+    function doButtonAltClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button3.click-Left 
+     */
+    function doButton3ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button4.click-Left 
+     */
+    function doButton4ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button5.click-Left 
+     */
+    function doButton5ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button8.click-Left 
+     */
+    function doButton8ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button6.click-Left 
+     */
+    function doButton6ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event button7.click-Left 
+     */
+    function doButton7ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+}
